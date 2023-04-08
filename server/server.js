bdc391316ce05fd6b3ebbab3f1f366e2a8702a51
husseinjfr <husseinmohammadjaafar@gmail.com>
2023-04-09 02:09:50 +0300

    #

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ app.use(cors());
 //static files
 app.use(express.static(path.join(__dirname, "../client/build")));
 
-+app.get("/*", function (req, res) {
+app.get("/", function (req, res) {
   res.sendFile(
     path.join(__dirname, "../client/build/index.html")
   );
